e565541f5251df9ad72c0777ce69c9c89964b92c
Mario Harper <mario_harper@volusion.com>
2016-10-03 14:21:43 -0500

    fix tf var reference

--- infrastructure/dev/main.tf
+++ infrastructure/dev/main.tf
@@ -6,6 +6,6 @@ module "api_gateway" {
   source          = "../modules/api_gateway"
   aws_account_id  = "${var.aws_account_id}"
   aws_region      = "${var.aws_region}"
-  api_stage       = "${var.api_stage}"
+  api_stage       = "${var.stage}"
   gateway_invoke_lambda_role_arn = "${module.iam.gateway_invoke_lambda_role_arn}"
 }
